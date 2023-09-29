b55ccf6bb73d55d3806f9ca687846bf5b8eb24a1
wellingtonflores <wellingtonluiscardoso.com>
2023-09-29 09:44:26 -0300

    criacao-classe-order_item

---
+++ order_item.py
@@ -0,0 +1,6 @@
+class OrderItem:
+    def __init__(self, quantity, unitary_price, order, product):
+        self._quantity = quantity
+        self._unitary_price = unitary_price
+        self._order = Order
+        self._product = Product
