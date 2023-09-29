63c6865368eeaaa482a6d4819554484589bea1bf
wellingtonflores <wellingtonluiscardoso.com>
2023-09-29 11:01:04 -0300

    criacao-classe-cliente_social_network

---
+++ client_social_network.py
@@ -0,0 +1,8 @@
+from multiprocessing.connection import Client
+from social_network import SocialNetwork
+from cliente import Cliente
+
+class ClientSocialNetwork:
+    def __init__(self, cliente, social_network):
+        self._cliente = Cliente
+        self._social_network = SocialNetwork
