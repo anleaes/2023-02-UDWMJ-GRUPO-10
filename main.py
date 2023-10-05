3170cb2849bea7a8ee86178d19de41cd1a95cfb2
wellingtonflores <wellingtonluiscardoso.com>
2023-10-05 10:02:57 -0300

    criacao de metodo cliente no main

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from cliente import Cliente
 def main():
-    pass
-
+    c1 = Cliente ("Wellington", "Flores", "Rua nao sei o que la", 51900000, "gostosinho@gmail.com", "M")
+    if c1._email == "gostosinho@gmail.com":
+        print("Email verificado é o gostosinho")
+    else:
+        print("Email não verificado")    
 if __name__ == "__main__":
     main()
