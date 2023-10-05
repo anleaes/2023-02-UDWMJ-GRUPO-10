cb3f3a89e5e6533a2a8f9892465c082edafeb374
thayseps <thaypereirasantos@gmail.com>
2023-10-05 10:30:49 -0300

    criac

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from unicodedata import category
+
+
+from category import Category
+
 def main():
-    pass
+    categoy1 = Category(1, "Esportes", "Suplementos e equipamentos esportivos")
 
 if __name__ == "__main__":
     main()
