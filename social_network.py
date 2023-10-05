de5e3e7810712d1f2f93db6acab83b1631f84c6e
Rasiver <gabrielmmarques28@gmail.com>
2023-10-05 10:56:48 -0300

    metodo socialnetwork

--- social_network.py
+++ social_network.py
@@ -2,4 +2,10 @@ class SocialNetwork:
     def __init__(self, name, description):
         self.name = name
         self.description = description
-        
+        pass
+
+    def mostrar_socialnetwork():
+        pass
+
+pessoa1 = SocialNetwork('Gabriel |','local: Porto Alegre')
+print(pessoa1.name, pessoa1.description)      
