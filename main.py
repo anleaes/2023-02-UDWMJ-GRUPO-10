f2a6362d6b3c1db3ef1f94cd577560e1d81df768
wellingtonflores <wellingtonluiscardoso.com>
2023-10-05 10:08:46 -0300

    importando todas as classes

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from cliente import Cliente
+from category import Category
+from client_social_network import ClientSocialNetwork
+from order_item import OrderItem
+from order import Order
+from product import Product
+from social_network import SocialNetwork
+
 def main():
     c1 = Cliente ("Wellington", "Flores", "Rua nao sei o que la", 51900000, "gostosinho@gmail.com", "M")
     if c1._email == "gostosinho@gmail.com":
