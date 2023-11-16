410aadd240d4441311fbc5bdcf1e391f4995a326
wellingtonflores <wellingtonluiscardoso.com>
2023-11-16 12:48:36 -0300

    configurando as urls

--- projetoapp/projetoapp/urls.py
+++ projetoapp/projetoapp/urls.py
@@ -21,5 +21,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('core.urls', namespace='core')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
