50b42028aa546f67653e1c8b8d5aaf04c74a709c
thayseps <thaypereirasantos@gmail.com>
2023-11-17 16:37:01 -0300

    On branch minha-parte2 Changes to be committed: 	modified:   apps/categories/__pycache__/__init__.cpython-312.pyc 	modified:   apps/categories/__pycache__/admin.cpython-312.pyc 	modified:   apps/categories/__pycache__/apps.cpython-312.pyc 	modified:   apps/categories/__pycache__/forms.cpython-312.pyc 	modified:   apps/categories/__pycache__/models.cpython-312.pyc 	modified:   apps/categories/__pycache__/urls.cpython-312.pyc 	modified:   apps/categories/__pycache__/views.cpython-312.pyc 	modified:   apps/categories/migrations/__pycache__/0001_initial.cpython-312.pyc 	modified:   apps/categories/migrations/__pycache__/__init__.cpython-312.pyc 	modified:   apps/clients/__pycache__/__init__.cpython-312.pyc 	modified:   apps/clients/__pycache__/admin.cpython-312.pyc 	modified:   apps/clients/__pycache__/apps.cpython-312.pyc 	modified:   apps/clients/__pycache__/forms.cpython-312.pyc 	modified:   apps/clients/__pycache__/models.cpython-312.pyc 	modified:   apps/clients/__pycache__/urls.cpython-312.pyc 	modified:   apps/clients/__pycache__/views.cpython-312.pyc 	modified:   apps/clients/migrations/__pycache__/0001_initial.cpython-312.pyc 	modified:   apps/clients/migrations/__pycache__/__init__.cpython-312.pyc 	modified:   apps/clients/urls.py 	modified:   apps/clients/views.py 	modified:   apps/core/__pycache__/__init__.cpython-312.pyc 	modified:   apps/core/__pycache__/admin.cpython-312.pyc 	modified:   apps/core/__pycache__/apps.cpython-312.pyc 	modified:   apps/core/__pycache__/models.cpython-312.pyc 	modified:   apps/core/__pycache__/urls.cpython-312.pyc 	modified:   apps/core/__pycache__/views.cpython-312.pyc 	modified:   apps/core/migrations/__pycache__/__init__.cpython-312.pyc 	modified:   apps/products/__pycache__/__init__.cpython-312.pyc 	modified:   apps/products/__pycache__/admin.cpython-312.pyc 	modified:   apps/products/__pycache__/apps.cpython-312.pyc 	modified:   apps/products/__pycache__/forms.cpython-312.pyc 	modified:   apps/products/__pycache__/models.cpython-312.pyc 	modified:   apps/products/__pycache__/urls.cpython-312.pyc 	modified:   apps/products/__pycache__/views.cpython-312.pyc 	modified:   apps/products/migrations/__pycache__/0001_initial.cpython-312.pyc 	modified:   apps/products/migrations/__pycache__/__init__.cpython-312.pyc 	modified:   apps/socialnetworks/__pycache__/__init__.cpython-312.pyc 	modified:   apps/socialnetworks/__pycache__/admin.cpython-312.pyc 	modified:   apps/socialnetworks/__pycache__/apps.cpython-312.pyc 	modified:   apps/socialnetworks/__pycache__/forms.cpython-312.pyc 	modified:   apps/socialnetworks/__pycache__/models.cpython-312.pyc 	modified:   apps/socialnetworks/__pycache__/urls.cpython-312.pyc 	modified:   apps/socialnetworks/__pycache__/views.cpython-312.pyc 	modified:   apps/socialnetworks/migrations/__pycache__/0001_initial.cpython-312.pyc 	modified:   apps/socialnetworks/migrations/__pycache__/__init__.cpython-312.pyc 	modified:   projetoapp/apps/clients/urls.py 	modified:   projetoapp/apps/clients/views.py 	modified:   templates/base.html 	modified:   templates/clients/list_clients.html

--- apps/clients/views.py
+++ apps/clients/views.py
@@ -47,3 +47,16 @@ def delete_client(request, id_client):
     client = Client.objects.get(id=id_client)
     client.delete()
     return redirect('clients:list_clients')
+
+def search_clients(request):
+    template_name = 'clients/list_clients.html'
+    query = request.GET.get('query')
+    client_socialnetworks = ClientSocialnetwork.objects.filter()
+    socialnetworks = Socialnetwork.objects.filter()
+    clients = Client.objects.filter(last_name__icontains=query)
+    context = {
+        'clients': clients,
+        'socialnetworks': socialnetworks,
+        'client_socialnetworks': client_socialnetworks
+    }
+    return render(request,template_name, context)
