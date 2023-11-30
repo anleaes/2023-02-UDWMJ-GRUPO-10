236b29df2fb18b48fd3dce83d191590dc0f14e92
wellingtonflores <wellingtonluiscardoso@hotmail.com>
2023-11-30 00:36:41 -0300

    tive que criar o core no main mesmo

--- modeloapp/modeloapp/settings.py
+++ modeloapp/modeloapp/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django-widget-tweaks',
+    'widget_tweaks',
+    
 ]
 
 MIDDLEWARE = [
