916daf7299165e8da3ea64fc97cb9c8e097764c6
wellingtonflores <wellingtonluiscardoso@hotmail.com>
2023-11-30 01:54:48 -0300

    editando a pasta de usuarios

--- modeloapp/apps/usuarios/urls.py
+++ modeloapp/apps/usuarios/urls.py
@@ -4,8 +4,9 @@ from . import views
 app_name = 'usuarios'
 
 urlpatterns = [
-    path('', views.list_usuarios, name='list_usuarios'),
-    path('adicionar/', views.add_usuarios, name='add_usuarios'),
-    path('editar/<int:id_usuario>/', views.edit_usuario, name='edit_usuario'),
-    path('excluir/<int:id_usuario>/', views.delete_usuario, name='delete_usuario'),
+    path('novo-usuario/',views.add_user, name='add_user'),
+    path('login/',views.user_login, name='user_login'),
+    path('sair/',views.user_logout, name='user_logout'),
+    path('alterar-senha/',views.user_change_password, name='user_change_password'),
+    path('alterar-usuario/<username>/',views.user_change_information, name='user_change_information'),
 ]
