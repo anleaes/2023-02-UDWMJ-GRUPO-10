bdd08bb6aba31afe2b5bf75af62049ee77537ff0
wellingtonflores <wellingtonluiscardoso@hotmail.com>
2023-11-30 21:35:36 -0300

    editando pasta avaliacao dos apps

--- modeloapp/apps/avaliacao/views.py
+++ modeloapp/apps/avaliacao/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+from .forms import AvaliacaoForm
+from .models import AvaliacaoModel
+
+def create_avaliacao(request):
+    if request.method == 'POST':
+        form = AvaliacaoForm(request.POST)
+        if form.is_valid():
+            nota = form.cleaned_data['nota']
+            avaliacao = AvaliacaoModel(nota)
+            avaliacao_resultado = avaliacao.avaliar()
+            return render(request, 'create_avaliacao.html', {'form': form, 'avaliacao_resultado': avaliacao_resultado})
+    else:
+        form = AvaliacaoForm()
+    return render(request, 'create_avaliacao.html', {'form': form})
