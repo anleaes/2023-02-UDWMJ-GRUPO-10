144c47d848a0ab40944d4d11b7ff161213b444b5
wellingtonflores <wellingtonluiscardoso@hotmail.com>
2023-12-06 00:06:59 -0300

    editando o endereco

--- modeloapp/apps/endereco/views.py
+++ modeloapp/apps/endereco/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .forms import EnderecoForm
 
 # Create your views here.
+
+def endereco_view(request):
+    if request.method == 'POST':
+        form = EnderecoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'success.html')
+    else:
+        form = EnderecoForm()
+    return render(request, 'endereco.html', {'form': form})
