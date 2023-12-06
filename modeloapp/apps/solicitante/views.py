2cb44a9228ea15fadfc7195520da4b4ad9c92041
wellingtonflores <wellingtonluiscardoso@hotmail.com>
2023-12-06 19:11:33 -0300

    editando solicitante

--- modeloapp/apps/solicitante/views.py
+++ modeloapp/apps/solicitante/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SolicitanteForm
 
 # Create your views here.
+
+def fazer_solicitacao(request):
+    if request.method == 'POST':
+        form = SolicitanteForm(request.POST)
+        if form.is_valid():
+            solicitante = form.save()
+            return redirect(request, 'sucesso.html', {'mensagem': 'Solicitação feita com sucesso!'})
+    else:
+        form = SolicitanteForm()
+
+    return redirect(request, 'fazer_solicitacao.html', {'form': form})
