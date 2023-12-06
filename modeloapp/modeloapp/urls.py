6dcb3a86c6499508f85b6680d2489cc8f1657a14
wellingtonflores <wellingtonluiscardoso@hotmail.com>
2023-12-06 14:46:59 -0300

    editando as urls

--- modeloapp/modeloapp/urls.py
+++ modeloapp/modeloapp/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('avaliacao/', include('avaliacao.urls', namespace='avaliacao')),
     path('servicos/', include('servicos.urls', namespace='servicos')),
     path('endereco/', include('endereco.urls', namespace='endereco')),
+    path('materiais/', include('materiais.urls', namespace='materiais')),
     
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
