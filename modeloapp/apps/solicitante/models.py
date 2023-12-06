37a6ea0ed447134a0b02d3cb40592f4c3c1de7ff
wellingtonflores <wellingtonluiscardoso@hotmail.com>
2023-12-06 20:22:24 -0300

    editando solicitante

--- modeloapp/apps/solicitante/models.py
+++ modeloapp/apps/solicitante/models.py
@@ -15,11 +15,3 @@ class Solicitacao(models.Model):
         verbose_name_plural = 'Solicitantes'
         ordering =['id']
 
-class Usuarios(models.Model):
-    solicitante = models.ForeignKey(Solicitante, on_delete=models.CASCADE)
-    
-    class Meta:
-        verbose_name = 'Solicitante'
-        verbose_name_plural = 'Solicitantes'
-        ordering =['id']
-
