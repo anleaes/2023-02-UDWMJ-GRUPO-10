19f5856258305b841670cde078f87b2182b9ba40
wellingtonflores <wellingtonluiscardoso@hotmail.com>
2023-12-06 15:31:41 -0300

    editando usuarios

--- modeloapp/apps/usuarios/models.py
+++ modeloapp/apps/usuarios/models.py
@@ -1,17 +1,17 @@
-from django import models
-from django import forms
+
+from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Usuario(models.Model):
+class Usuarios(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 class Solicitacao(models.Model):
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
     
 class Atendimento(models.Model):
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
     
     class Meta:
         verbose_name = 'Usuarios'
