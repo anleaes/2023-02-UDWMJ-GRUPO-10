0d32ba0ef9fcf4d74af7e2e7ff37f30acadccff0
thayseps <thaypereirasantos@gmail.com>
2023-12-06 18:40:20 -0300

    arrumando atendimento

--- modeloapp/apps/atendimento/forms.py
+++ modeloapp/apps/atendimento/forms.py
@@ -2,7 +2,8 @@ from django import forms
 from .models import Atendimento
 
 class AtendimentoForm(forms.ModelForm):
-
+    data = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+    hora = forms.TimeField(widget=forms.TimeInput(attrs={'type': 'time'}))
     class Meta:
         model = Atendimento
         exclude = ('created_on' , 'updated_on',)
