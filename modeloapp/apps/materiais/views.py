08d5da1910364af76b40791d57985ffeac7f9853
wellingtonflores <wellingtonluiscardoso@hotmail.com>
2023-12-06 14:44:57 -0300

    editando materiais

--- modeloapp/apps/materiais/views.py
+++ modeloapp/apps/materiais/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from modeloapp.apps.materiais.models import Materiais
 
 # Create your views here.
+
+from .forms import MateriaisForm
+
+def lista_materiais(request):
+    materiais = Materiais.objects.all()
+    return render(request, 'materiais/lista_materiais.html', {'materiais': materiais})
+
+def adiciona_material(request):
+    if request.method == 'POST':
+        form = MateriaisForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_materiais')
+    else:
+        form = MateriaisForm()
+    return render(request, 'materiais/adiciona_material.html', {'form': form})
