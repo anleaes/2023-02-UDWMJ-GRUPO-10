b9457f08af12e3366baefd355fba666a89292a47
wellingtonflores <wellingtonluiscardoso@hotmail.com>
2023-12-06 22:38:59 -0300

    editando usuarios

--- modeloapp/apps/usuarios/models.py
+++ modeloapp/apps/usuarios/models.py
@@ -1,6 +1,31 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from solicitante.models import Solicitante
 
-class Usuario(AbstractUser):
-    id = models.AutoField(primary_key=True)
-    email = models.EmailField(max_length=254, unique=True)
+class Usuarios(models.Model):
+    usuarios = models.CharField('Usuario', max_length=50)
+    email = models.EmailField('E-mail',null=False, blank=False)
+    senha = models.CharField('Senha', max_length=50)
+    usuarios_solicitante = models.ManyToManyField(Solicitante, through='UsuariosSolicitante', blank=True)
+    
+    class Meta:
+        verbose_name = 'Usuario'
+        verbose_name_plural = 'Cadastro de Usuarios'
+        ordering =['id']
+
+    def __str__(self):
+        return self.email
+    
+class UsuariosSolicitante(models.Model):
+    nome = models.CharField('Nome', max_length=50) 
+    sobrenome = models.CharField('Sobrenome', max_length=50)
+    cpf = models.CharField('CPF', max_length=50)
+    usuarios = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
+    solicitante = models.ForeignKey(Solicitante, on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name = 'UsuariosSolicitante'
+        verbose_name_plural = 'UsuariosSolicitantes'
+        ordering =['id']
+
+    def __str__(self):
+        return self.solicitante.nome
